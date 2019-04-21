f7dbdbe2aeeb9457ed19860e59be1723b3bb19e6
Nkoyan <yoann.kergall@gmail.com>
2019-04-21 01:55:01 +0100

    update config to use env variables

--- bin/deploy/deploy.sh
+++ bin/deploy/deploy.sh
@@ -1,10 +1,6 @@
 sudo rm /var/www/micropost && \
 sudo ln -s /var/www/micropost_current /var/www/micropost && \
 cd /var/www/micropost && \
-sed -ri "s/^APP_ENV=.*$/APP_ENV=$APP_ENV/" .env && \
-sed -ri "s/^DATABASE_URL=.*$/DATABASE_URL=$DATABASE_URL/" .env && \
-sed -ri "s/^MAILER_URL=.*$/MAILER_URL=$MAILER_URL/" .env && \
-sed -ri "s/^MAILER_FROM=.*$/MAILER_FROM=$MAILER_FROM/" .env && \
 sudo php bin/console doctrine:migrations:migrate --no-interaction && \
 sudo chown -R www-data:www-data /var/www/micropost_current && \
 sudo chown -h www-data:www-data /var/www/micropost && \
